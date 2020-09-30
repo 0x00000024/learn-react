fa4fd4bbf0133b5a061aad8f80591080d5af6919
Ethan Yu <sohbmpethan@gmail.com>
2020-09-30 20:02:48 +0900

    03-Passing-and-Receiving-Props

---
+++ 03-Communicating-with-Props/03-Passing-and-Receiving-Props/components/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <CommentDetail author="Sam" />
+      <CommentDetail author="Alex" />
+      <CommentDetail author="Jane" />
+    </div>
+  );
+};
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root'),
+);
