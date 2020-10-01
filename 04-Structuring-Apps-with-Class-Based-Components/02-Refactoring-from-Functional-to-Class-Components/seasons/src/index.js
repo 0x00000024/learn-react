14096b6d03833799cea3cce978eb83dbb089fd8d
Ethan Yu <sohbmpethan@gmail.com>
2020-10-01 14:11:20 +0900

    02-Refactoring-from-Functional-to-Class-Components

---
+++ 04-Structuring-Apps-with-Class-Based-Components/02-Refactoring-from-Functional-to-Class-Components/seasons/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// const App = () => {
+//   window.navigator.geolocation.getcurrentposition(
+//       (position) => console.log(position),
+//       (err) => console.log(err),
+//   );
+//   return <div>hi there!</div>;
+// };
+
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err),
+    );
+    return <div>hi there!</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
