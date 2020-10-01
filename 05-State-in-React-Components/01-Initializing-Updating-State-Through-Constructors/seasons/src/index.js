afb9932e56d98fec426d58a7b301797b0e767289
Ethan Yu <sohbmpethan@gmail.com>
2020-10-01 15:54:59 +0900

    01-Initializing-Updating-State-Through-Constructors

---
+++ 05-State-in-React-Components/01-Initializing-Updating-State-Through-Constructors/seasons/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // THIS IS THE ONLY TIME we do direct assignment
+    // to this.state
+    this.state = {lat: null};
+
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+          // we called setstate!!!!
+          this.setState({lat: position.coords.latitude});
+        },
+        (err) => console.log(err),
+    );
+  }
+
+  // React says we have to define render!!
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
