7b48ffd00a141a7c5a93f58ffd57f81942dd6446
Ethan Yu <sohbmpethan@gmail.com>
2020-10-01 20:02:36 +0900

    04-Extracting-Options-to-Config-Objects

---
+++ 06-Understanding-Lifecycle-Methods/04-Extracting-Options-to-Config-Objects/seasons/src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // THIS IS THE ONLY TIME we do direct assignment
+    // to this.state
+    this.state = {lat: null, errorMessage: ''};
+  }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => this.setState({lat: position.coords.latitude}),
+        (err) => this.setState({errorMessage: err.message}),
+    );
+  }
+
+  // React says we have to define render!!
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <div>Loading!</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
