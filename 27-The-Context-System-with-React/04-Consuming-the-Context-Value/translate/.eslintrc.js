ad9a2bcc562409876df7ce84f0ad77ba4f5a057e
Ethan Yu <sohbmpethan@gmail.com>
2020-11-23 17:44:39 +0900

    04-Consuming-the-Context-Value

---
+++ 27-The-Context-System-with-React/04-Consuming-the-Context-Value/translate/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+  },
+  'extends': [
+    'plugin:react/recommended',
+    'google',
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+  },
+  'parser': 'babel-eslint',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+  ],
+  'rules': {
+    'require-jsdoc': 0,
+    'react/prop-types': 0,
+    'no-invalid-this': 0,
+  },
+  'settings': {
+    'react': {
+      'version': 'detect',
+    },
+  },
+};
