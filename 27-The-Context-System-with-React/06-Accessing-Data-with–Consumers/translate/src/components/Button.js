83966c75c25e3ae3eeb715e0e7de92b75294a3d6
Ethan Yu <sohbmpethan@gmail.com>
2020-11-23 17:45:10 +0900

    06-Accessing-Data-with–Consumers

---
+++ 27-The-Context-System-with-React/06-Accessing-Data-with–Consumers/translate/src/components/Button.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+
+class Button extends React.Component {
+  renderSubmit(value) {
+    return value === 'english' ? 'Submit' : 'Voorleggen';
+  }
+
+  render() {
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {(value) => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
+}
+
+export default Button;
