d598658ee3c5442c6c51649781b302551f67b2af
Ethan Yu <sohbmpethan@gmail.com>
2021-03-23 23:50:54 +0900

    10-List-Building

---
+++ 12-Understanding-Hooks-in-React/10-List-Building/widgets/src/components/Search.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {useState} from 'react';
+import {useEffect} from 'react';
+import axios from 'axios';
+
+const Search = () => {
+  const [term, setTerm] = useState('programming');
+  const [results, setResults] = useState([]);
+
+  console.log(results);
+
+  useEffect(() => {
+    const search = async () => {
+      const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+    search();
+  }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            GO
+          </a>
+        </div>
+        <div className="header">{result.title}</div>
+        {result.snippet}
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter search term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+      <div className="ui celled list">{renderedResults}</div>
+    </div>
+  );
+};
+
+export default Search;
