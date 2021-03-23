e3f7335219e495354e272a156219fe3834826583
Ethan Yu <sohbmpethan@gmail.com>
2021-03-23 22:13:10 +0900

    08-Async-Code-in-useEffect

---
+++ 12-Understanding-Hooks-in-React/08-Async-Code-in-useEffect/widgets/src/components/Search.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {useState} from 'react';
+import {useEffect} from 'react';
+import axios from 'axios';
+
+const Search = () => {
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    const search = async () => {
+      await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+    };
+    search();
+  }, [term]);
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter search term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
