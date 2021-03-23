782887b85063eb95cef7fe1f19d69a032077eadb
Ethan Yu <sohbmpethan@gmail.com>
2021-03-23 23:08:43 +0900

    09-Default-Search-Terms

---
+++ 12-Understanding-Hooks-in-React/09-Default-Search-Terms/widgets/src/components/Search.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {useState} from 'react';
+import {useEffect} from 'react';
+import axios from 'axios';
+
+const Search = () => {
+  const [term, setTerm] = useState('programming');
+  const [results, setResults] = useState([]);
+
+  console.log(results);
+
+  useEffect(() => {
+    const search = async () => {
+      const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+    search();
+  }, [term]);
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter search term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
